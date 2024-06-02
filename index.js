0296e0bfa3b99766c072171ed628b910adebae94
MariaLedier <mariaclara.ledierdelp@gmail.com>
2024-06-02 20:34:51 -0300

    Finalizando

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import express from "express";
 import path from "path";
+import session from "express-session";
+//Não é o HTTP que agora terá o estado e sim a nossa aplicação.
+//Esse estado é conhecido como sessão.
+
+//Para permitir que informações sejam mantidas e trocadas entre cliente e servidor,
+//tais informações podem ser armazenadas em cookies.
+//Para possibilitar que nossa aplicação manipule cookies será necessário instalar
+//o módulo cookieParser (npm install cookie-parser);
+import cookieParser from "cookie-parser";
 
 const host = "0.0.0.0";
 const porta = 3000;
@@ -7,13 +16,65 @@ const porta = 3000;
 let listaProdutos = [];
 
 const app = express();
-// Configurar o express para manipular corretamente os dados
-// quando eles forem submetidos via método POST
-app.use(express.urlencoded({ extended: true })); // Habilita a biblioteca QueryString
 
+// Configuração da sessão
+app.use(
+  session({
+    secret: "MinH4Ch4v3S3cr3t4", //chave para assinar os dados da sessão
+    resave: true, //salva a sessão a cada requisição HTTP
+    saveUninitialized: true,
+    cookie: {
+      //tempo de vida da sessão
+      maxAge: 1000 * 60 * 15, //15 minutos
+    },
+  })
+);
+// Cookie Parser
+app.use(cookieParser());
+
+// Configuração para manipular dados POST
+app.use(express.urlencoded({ extended: true }));
+
+// Servir arquivos estáticos
 app.use(express.static(path.join(process.cwd(), "publico")));
 
-function cadastrarProduto(requisicao, resposta) {
+// Rota raiz redireciona para a página de login
+app.get("/", (req, res) => {
+  res.redirect("/login");
+});
+
+// Página de login
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "publico", "login.html"));
+});
+
+// Processamento do login
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // Autenticação
+  if (username === "usuario" && password === "senha") {
+    req.session.username = username;
+    res.redirect("/cadastrarProduto");
+  } else {
+    res.send(
+      'Usuário ou senha inválidos. <a href="/login">Tente novamente</a>.'
+    );
+  }
+});
+
+// Página de cadastro de produtos
+app.get("/cadastrarProduto", (req, res) => {
+  if (!req.session.username) {
+    res.send('Você precisa realizar o login. <a href="/login">Login</a>');
+  } else {
+    res.sendFile(
+      path.join(process.cwd(), "protegido", "cadastrarProduto.html")
+    );
+  }
+});
+
+function cadastrarProduto(req, res) {
   const {
     codigoBarras,
     descricaoProduto,
@@ -22,187 +83,88 @@ function cadastrarProduto(requisicao, resposta) {
     dataValidade,
     qtdEstoque,
     nomeFabricante,
-  } = requisicao.body;
+  } = req.body;
 
-  // Verificando se os campos foram preenchidos (não estão vazios)
-  let erros = {};
-  if (!codigoBarras)
-    erros.codigoBarras = "Por favor, informe o código de barras.";
-  if (!descricaoProduto)
-    erros.descricaoProduto = "Por favor, informe a descrição do produto.";
-  if (!precoCusto) erros.precoCusto = "Por favor, informe o preço de custo.";
-  if (!precoVenda) erros.precoVenda = "Por favor, informe o preço de venda.";
-  if (!dataValidade)
-    erros.dataValidade = "Por favor, informe a data de validade.";
-  if (!qtdEstoque)
-    erros.qtdEstoque = "Por favor, informe a quantidade em estoque.";
-  if (!nomeFabricante)
-    erros.nomeFabricante = "Por favor, informe o nome do fabricante.";
-
-  if (Object.keys(erros).length === 0) {
-    listaProdutos.push({
-      codigoBarras,
-      descricaoProduto,
-      precoCusto,
-      precoVenda,
-      dataValidade,
-      qtdEstoque,
-      nomeFabricante,
-    });
-    resposta.redirect("/listarProdutos");
+  if (
+    codigoBarras &&
+    descricaoProduto &&
+    precoCusto &&
+    precoVenda &&
+    dataValidade &&
+    qtdEstoque &&
+    nomeFabricante
+  ) {
+    if (parseFloat(precoVenda) > parseFloat(precoCusto)) {
+      listaProdutos.push({
+        codigoBarras,
+        descricaoProduto,
+        precoCusto,
+        precoVenda,
+        dataValidade,
+        qtdEstoque,
+        nomeFabricante,
+      });
+      res.redirect("/listarProdutos");
+    } else {
+      res.send(
+        'O preço de venda deve ser maior que o preço de custo. <a href="/cadastrarProduto">Tente novamente</a>.'
+      );
+    }
   } else {
-    resposta.write(`
-        <!DOCTYPE html>
-        <html lang="en">
-        
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Página de Cadastro de Produtos</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-                integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        </head>
-        
-        <body>
-            <div class="container m-5">
-                <form method="POST" action='/cadastrarProduto' class="border row g-3 needs-validation" novalidate>
-                    <legend>Cadastro de Produtos</legend>
-                    <div class="col-md-4">
-                        <label for="codigoBarras" class="form-label">Código de barras:</label>
-                        <input type="text" class="form-control" id="codigoBarras" name="codigoBarras" value="${codigoBarras}" required>
-                        ${
-                          erros.codigoBarras
-                            ? `<div class="alert alert-danger mt-2" role="alert">${erros.codigoBarras}</div>`
-                            : ""
-                        }
-                    </div>
-                    <div class="col-md-4">
-                        <label for="descricaoProduto" class="form-label">Descrição do produto:</label>
-                        <input type="text" class="form-control" id="descricaoProduto" name="descricaoProduto" value="${descricaoProduto}" required>
-                        ${
-                          erros.descricaoProduto
-                            ? `<div class="alert alert-danger mt-2" role="alert">${erros.descricaoProduto}</div>`
-                            : ""
-                        }
-                    </div>
-                    <div class="col-md-4">
-                        <label for="precoCusto" class="form-label">Preço de custo:</label>
-                        <div class="input-group has-validation">
-                            <span class="input-group-text" id="inputGroupPrepend">R$</span>
-                            <input type="text" class="form-control" id="precoCusto" name="precoCusto" value="${precoCusto}" aria-describedby="inputGroupPrepend" required>
-                        </div>
-                        ${
-                          erros.precoCusto
-                            ? `<div class="alert alert-danger mt-2" role="alert">${erros.precoCusto}</div>`
-                            : ""
-                        }
-                    </div>
-                    <div class="col-md-4">
-                        <label for="precoVenda" class="form-label">Preço de venda:</label>
-                        <div class="input-group has-validation">
-                            <span class="input-group-text" id="inputGroupPrepend">R$</span>
-                            <input type="text" class="form-control" id="precoVenda" name="precoVenda" value="${precoVenda}" aria-describedby="inputGroupPrepend" required>
-                        </div>
-                        ${
-                          erros.precoVenda
-                            ? `<div class="alert alert-danger mt-2" role="alert">${erros.precoVenda}</div>`
-                            : ""
-                        }
-                    </div>
-                    <div class="col-md-4">
-                        <label for="dataValidade" class="form-label">Data de validade:</label>
-                        <input type="date" class="form-control" id="dataValidade" name="dataValidade" value="${dataValidade}" required>
-                        ${
-                          erros.dataValidade
-                            ? `<div class="alert alert-danger mt-2" role="alert">${erros.dataValidade}</div>`
-                            : ""
-                        }
-                    </div>
-                    <div class="col-md-4">
-                        <label for="qtdEstoque" class="form-label">Qtd em estoque:</label>
-                        <input type="number" class="form-control" id="qtdEstoque" name="qtdEstoque" value="${qtdEstoque}" required>
-                        ${
-                          erros.qtdEstoque
-                            ? `<div class="alert alert-danger mt-2" role="alert">${erros.qtdEstoque}</div>`
-                            : ""
-                        }
-                    </div>
-                    <div class="col-md-4">
-                        <label for="nomeFabricante" class="form-label">Nome do fabricante:</label>
-                        <input list="fabricantes" class="form-control" id="nomeFabricante" name="nomeFabricante" value="${nomeFabricante}" required>
-                        <datalist id="fabricantes">
-                            <option value="Fabricante A">
-                            <option value="Fabricante B">
-                            <option value="Fabricante C">
-                            <option value="Fabricante D">
-                            <option value="Fabricante E">
-                        </datalist>
-                        ${
-                          erros.nomeFabricante
-                            ? `<div class="alert alert-danger mt-2" role="alert">${erros.nomeFabricante}</div>`
-                            : ""
-                        }
-                    </div>
-                    <div class="col-12 mb-3">
-                        <button class="btn btn-primary" type="submit">Cadastrar</button>
-                        <a class="btn btn-secondary" href="/">Voltar</a>                   
-                    </div>
-                </form>
-            </div>
-        </body>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-        </html>`);
-    resposta.end(); // Finaliza o envio da resposta!
+    res.send(
+      'Todos os campos são obrigatórios. <a href="/cadastrarProduto">Tente novamente</a>.'
+    );
   }
 }
 
-// Quando um usuário enviar uma requisição do tipo POST
-// para o endpoint 'http://localhost:3000/cadastrarProduto'
-// executa a função 'cadastrarProduto()'
+// Rota para cadastrar produto
 app.post("/cadastrarProduto", cadastrarProduto);
 
-app.get("/listarProdutos", (req, resp) => {
-  resp.write("<html>");
-  resp.write("<head>");
-  resp.write("<title>Resultado do cadastro</title>");
-  resp.write('<meta charset="utf-8">');
-  resp.write(
-    '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">'
-  );
-  resp.write("</head>");
-  resp.write("<body>");
-  resp.write("<h1>Lista de Produtos</h1>");
-  resp.write('<table class="table table-striped">');
-  resp.write("<tr>");
-  resp.write("<th>Código de Barras</th>");
-  resp.write("<th>Descrição do Produto</th>");
-  resp.write("<th>Preço de Custo</th>");
-  resp.write("<th>Preço de Venda</th>");
-  resp.write("<th>Data de Validade</th>");
-  resp.write("<th>Qtd em Estoque</th>");
-  resp.write("<th>Nome do Fabricante</th>");
-  resp.write("</tr>");
-  for (let i = 0; i < listaProdutos.length; i++) {
-    resp.write("<tr>");
-    resp.write(`<td>${listaProdutos[i].codigoBarras}</td>`);
-    resp.write(`<td>${listaProdutos[i].descricaoProduto}</td>`);
-    resp.write(`<td>${listaProdutos[i].precoCusto}</td>`);
-    resp.write(`<td>${listaProdutos[i].precoVenda}</td>`);
-    resp.write(`<td>${listaProdutos[i].dataValidade}</td>`);
-    resp.write(`<td>${listaProdutos[i].qtdEstoque}</td>`);
-    resp.write(`<td>${listaProdutos[i].nomeFabricante}</td>`);
-    resp.write("</tr>");
+// Rota para listar produtos cadastrados
+app.get("/listarProdutos", (req, res) => {
+  if (!req.session.username) {
+    res.send('Você precisa realizar o login. <a href="/login">Login</a>');
+  } else {
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<title>Produtos Cadastrados</title>");
+    res.write('<meta charset="utf-8">');
+    res.write(
+      '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">'
+    );
+    res.write("</head>");
+    res.write("<body>");
+    res.write("<h1>Lista de Produtos</h1>");
+    res.write('<table class="table table-striped">');
+    res.write("<tr>");
+    res.write("<th>Código de Barras</th>");
+    res.write("<th>Descrição</th>");
+    res.write("<th>Preço de Custo</th>");
+    res.write("<th>Preço de Venda</th>");
+    res.write("<th>Data de Validade</th>");
+    res.write("<th>Quantidade em Estoque</th>");
+    res.write("<th>Nome do Fabricante</th>");
+    res.write("</tr>");
+    for (let i = 0; i < listaProdutos.length; i++) {
+      res.write("<tr>");
+      res.write(`<td>${listaProdutos[i].codigoBarras}</td>`);
+      res.write(`<td>${listaProdutos[i].descricaoProduto}</td>`);
+      res.write(`<td>${listaProdutos[i].precoCusto}</td>`);
+      res.write(`<td>${listaProdutos[i].precoVenda}</td>`);
+      res.write(`<td>${listaProdutos[i].dataValidade}</td>`);
+      res.write(`<td>${listaProdutos[i].qtdEstoque}</td>`);
+      res.write(`<td>${listaProdutos[i].nomeFabricante}</td>`);
+      res.write("</tr>");
+    }
+    res.write("</table>");
+    res.write('<a href="/cadastrarProduto">Cadastrar novo produto</a>');
+    res.write("</body>");
+    res.write(
+      '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>'
+    );
+    res.write("</html>");
+    res.end();
   }
-  resp.write("</table>");
-  resp.write('<a href="/">Voltar</a>');
-  resp.write("</body>");
-  resp.write(
-    '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>'
-  );
-  resp.write("</html>");
-  resp.end();
 });
 
 app.listen(porta, host, () => {
